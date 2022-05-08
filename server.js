4082058d3d4a7a8f162430c758428de0e65ba8fe
Kristina Pang <kvypang@gmail.com>
2022-05-07 17:02:41 -0700

    connect sequelize sessionm set up express app

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const allRoutes = require("./controllers");
+const session = require("express-session");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const { User, Blog } = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use("/", allRoutes);
+
+sequelize.sync({ force: false }).then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
