50d696d4db20db1f601e22f63f01db17c3d2c388
Kristina Pang <kvypang@gmail.com>
2022-05-08 14:32:12 -0700

    path to all routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,14 @@
 const router = express.Router();
-const express = require('express');
+// const express = require('express');
+
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
+
+// api, home page, and dashboard routes
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
+
